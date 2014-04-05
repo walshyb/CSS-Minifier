fe02aed720faa93404247a7bd2746c425b3b39b7
walshyb <walshb1@hawkmail.newpaltz.edu>
2014-04-05 02:24:43 -0400

    Simplified so that no null characters are printed

--- minifier.php
+++ minifier.php
@@ -6,30 +6,39 @@
     
     <body>
         <!--Design working, but still under development; style then follows-->
-        <form name="form" method="POST">
-            <textarea id="input" name="input" rows="15" wrap="off" spellcheck="false" style="margin: 0px; height: 310px; width: 555px;">
-            </textarea>
-
-            <input type="submit" value="Minify!"/>
+        <h1>CSS Minifier</h1>
             
-        </form>
-        
-        <code>
-            <?php  
-                $code = $_POST['input'];
-                for($i = 0; $i < strlen($code); $i++)
-                {
-                    if($code[$i] == " " || $code[$i] == "\n" || $code[$i] == "\t" || $code[$i] == "\r")
-                    {
-                        $code[$i] = "";
+            <table>
+                <tr>
+                    <td>
+                        <h3 style="float: left">Input CSS</h3>
                         
-                    }
-                    elseif($code[$i] == "\0"){}
-    
+                        <div id="in">
+                            <form name="form" method="POST">
+                            <textarea id="input" name="input" rows="15" wrap="off" spellcheck="false" style="margin: 0px; height: 265px; width: 370px;"></textarea><br>
+                            <input type="submit" value="Minify!"/>
+                            </form>
+                        </div></td>
                     
-                    echo "$code[$i]";
-                }
-            ?>
-        </code>  
+                     <td>
+                        <div id="out">
+                            <h3 style="float: left">Minified Output</h3>
+                            <textarea id="output" style="margin: 0px; height: 265px; width: 370px;"><?php  
+                            $code = $_POST['input'];
+                            for($i = 0; $i < strlen($code); $i++)
+                            {
+                                if($code[$i] == " " || $code[$i] == "\n" || $code[$i] == "\t" || $code[$i] == "\r")
+                                {                                                   
+                                }
+                                else
+                                {
+                                    echo $code[$i];
+                                }
+                            }
+                        ?></textarea> 
+                        </div>
+                    </td>
+                </tr>
+            </table> 
     </body>
 </head>
